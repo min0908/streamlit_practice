6ef8e7d2ce632b1d046c55ee1a9c8ac82765458f
min0908 <94737255+min0908@users.noreply.github.com>
2023-05-12 15:26:39 +0900

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,24 @@
+import streamlit as st
+st.set_page_config(page_title='datathon', layout="wide", initial_sidebar_state="collapsed")
+
+
+STYLE = """
+#key {
+    color: #000;
+    background-color: #FAF3DD;
+    text-decoration: none;
+}
+
+.title {
+    font-size: 24px;
+    margin-top: 10px;
+    font-weight: medium;
+}
+
+.callout {
+    padding: 20px;
+    background-color: #F8F8F8;
+    border-left: 4px solid #3B81F5;
+    margin: 1em 0em;
+    color: black;
+}
